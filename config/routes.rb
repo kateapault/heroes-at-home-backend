9b0434ffb7c10771571c556cf7f6ca83fadc58ab
github username <github email address>
2020-04-20 15:09:15 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :addresses
+  resources :categories
+  resources :equipments
+  resources :materials
+  resources :orders
+  resources :parts
+
+  post 'orders/:id/change-status', to: 'orders#change_status'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
